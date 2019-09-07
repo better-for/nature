d8c0744c01da007879175128ff1961734128a2be
JEONGJIHUN <lippin21@gmail.com>
2019-09-07 23:41:22 +0900

    <feat>(OG):Add og tag

---
+++ packages/client-web/pages/OG/index.tsx
@@ -0,0 +1,34 @@
+import React, { FC } from 'react';
+
+export const OGHome: FC = () => (
+  <>
+    <title>Better for</title>
+    <meta
+      key="description"
+      name="description"
+      content={`SAVE THE WORLD, SAVE THE YOURSELF.`}
+    />
+    <meta
+      key="image_src"
+      name="image_src"
+      content={
+        'https://postfiles.pstatic.net/MjAxOTA5MDdfMjc5/MDAxNTY3ODYxODA1MTIz.LpUixWaK3qEzhKA23EjEThjN0C81uwl6Ec9ii0-CGQAg.OFNWw1kvH1ugW_dZV_vqdK9Odnedcr9dpLXj-j5Jg7Ig.PNG.ggijnd/swsy_ver_2.png?type=w966'
+      }
+    />
+    <meta key="og:type" name="og:type" content={`website`} />
+    <meta key="og:title" name="og:title" content={`Better for`} />
+    <meta
+      key="og:description"
+      name="og:description"
+      content={`SAVE THE WORLD, SAVE THE YOURSELF.`}
+    />
+    <meta key="og:url" name="og:url" content={'url'} />
+    <meta
+      key="og:image"
+      name="og:image"
+      content={
+        'https://postfiles.pstatic.net/MjAxOTA5MDdfMjc5/MDAxNTY3ODYxODA1MTIz.LpUixWaK3qEzhKA23EjEThjN0C81uwl6Ec9ii0-CGQAg.OFNWw1kvH1ugW_dZV_vqdK9Odnedcr9dpLXj-j5Jg7Ig.PNG.ggijnd/swsy_ver_2.png?type=w966'
+      }
+    />
+  </>
+);
