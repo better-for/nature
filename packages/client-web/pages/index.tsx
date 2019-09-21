92b1ae37f9f603826fa5fbb9f3114759b4a84745
letsget23 <letsget23@gmail.com>
2019-09-21 20:31:07 +0900

    <feat>(Mobx): Add MobX observers

--- packages/client-web/pages/index.tsx
+++ packages/client-web/pages/index.tsx
@@ -14,33 +14,43 @@ import {
 } from './style';
 import { LINK_LIST } from '../temporaryData/index';
 import { OGHome } from './OG/index';
+import Nav from '../components/Nav/Nav';
+import { useStore } from '../src/utils/storeUtils';
+import { observer } from 'mobx-react';
 
-const Home = () => (
-  <>
-    <Head>
-      <OGHome />
-    </Head>
-    <Main>
-      <TitleSection>
-        <Title>
-          Better For <Point>Nature</Point>
-        </Title>
-      </TitleSection>
-      <Description>
-        <code>SAVE THE WORLD, SAVE THE YOURSELF.</code>
-      </Description>
-      <Row>
-        {LINK_LIST.map(({ link, title, description }) => (
-          <Link href={link} key={title}>
-            <Card>
-              <ItemTitle>{title} &rarr;</ItemTitle>
-              <ItemDescription>{description}</ItemDescription>
-            </Card>
-          </Link>
-        ))}
-      </Row>
-    </Main>
-  </>
-);
+const Home = observer(() => {
+  const { uiStore } = useStore();
+  const { navigation } = uiStore;
+
+  return (
+    <>
+      <Head>
+        <OGHome />
+      </Head>
+      <Main>
+        <button onClick={navigation.toggle}>🍔</button>
+        {navigation.opened && <Nav />}
+        <TitleSection>
+          <Title>
+            Better For <Point>Nature</Point>
+          </Title>
+        </TitleSection>
+        <Description>
+          <code>SAVE THE WORLD, SAVE THE YOURSELF.</code>
+        </Description>
+        <Row>
+          {LINK_LIST.map(({ link, title, description }) => (
+            <Link href={link} key={title}>
+              <Card>
+                <ItemTitle>{title} &rarr;</ItemTitle>
+                <ItemDescription>{description}</ItemDescription>
+              </Card>
+            </Link>
+          ))}
+        </Row>
+      </Main>
+    </>
+  );
+});
 
 export default Home;
