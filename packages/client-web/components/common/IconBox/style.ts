bfe6dabba7c5d66161dafc3466fd820469b24482
JEONGJIHUN <lippin21@gmail.com>
2019-10-12 22:31:37 +0900

    <chore>(style):Fix Icon style

--- packages/client-web/components/common/IconBox/style.ts
+++ packages/client-web/components/common/IconBox/style.ts
@@ -5,6 +5,8 @@ export const StyledIcon = styled.div<IIcon>`
   width: ${({ size }) => size};
   height: ${({ size }) => size};
   font-size: ${({ size }) => size};
+  line-height: 1;
   vertical-align: middle;
+  text-align: right;
   border-radius: 50%;
 `;
