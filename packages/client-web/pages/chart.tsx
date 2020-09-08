d9ae8ae17ad559d4ed15e6a25a43d4359af41c6c
JEONGJIHUN <lippin21@gmail.com>
2020-09-09 00:38:11 +0900

    fix(): remove other site code

--- packages/client-web/pages/chart.tsx
+++ packages/client-web/pages/chart.tsx
@@ -1,9 +1,9 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { observer } from 'mobx-react';
 import 'mobx-react-lite/batchingForReactDom';
 import { I18nPage, useTranslation } from 'I18n';
 
-import { Main, ChartContainer } from 'src/style';
+import { Main } from 'src/style';
 import {
   MainTitle,
   FoodChart,
@@ -12,49 +12,21 @@ import {
   CarbonFootPrint
 } from 'src/components';
 
-declare global {
-  interface Window {
-    Highcharts: null;
-  }
-}
+const Chart: I18nPage = observer(() => {
+  const { t } = useTranslation();
 
-const Chart: I18nPage<{ isDarkTheme: boolean }> = observer(
-  ({ isDarkTheme }) => {
-    const { t } = useTranslation();
-    const theme = isDarkTheme ? 'dark-unica' : 'default';
-    const scriptSrc = `https://www.climatelevels.org/graphs/js/co2.php?theme=${theme}&pid=2degreesinstitute`;
-
-    useEffect(() => {
-      const script = document.createElement('script');
-      script.type = 'text/javascript';
-      script.src = scriptSrc;
-      script.async = true;
-
-      document.body.appendChild(script);
-
-      return () => {
-        document.body.removeChild(script);
-        window.Highcharts = null;
-      };
-    }, [isDarkTheme]);
-
-    return (
-      <>
-        <Main>
-          <MainTitle>{t('Climatic change-related chart')}</MainTitle>
-        </Main>
-        <GreenhouseGas isDarkTheme={isDarkTheme} />
-        <GreenhouseEffect isDarkTheme={isDarkTheme} />
-        <CarbonFootPrint isDarkTheme={isDarkTheme} />
-
-        <FoodChart />
-        <ChartContainer>
-          <div id="co2-widget-container"></div>
-        </ChartContainer>
-      </>
-    );
-  }
-);
+  return (
+    <>
+      <Main>
+        <MainTitle>{t('Climatic change-related chart')}</MainTitle>
+      </Main>
+      <GreenhouseGas />
+      <GreenhouseEffect />
+      <CarbonFootPrint />
+      <FoodChart />
+    </>
+  );
+});
 
 export default Chart;
 
